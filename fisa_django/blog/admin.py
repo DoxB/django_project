5a7cf866785b9779595c882744345dca312977cd
DoxB <jg.doxb@gmail.com>
2024-08-27 16:25:38 +0900

    240827_study

--- fisa_django/blog/admin.py
+++ fisa_django/blog/admin.py
@@ -1,7 +1,9 @@
 #blog/admin.py
 
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, Tag
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Tag)
